ea3395f3fda43f532f087b31d9e4ca1dd52cb712
aion0721 <rudolf.aion@gmail.com>
2024-07-14 12:38:29 +0900

    feat: set questions

--- src/questions.ts
+++ src/questions.ts
@@ -13,5 +13,10 @@ export const questions: Question[] = [
     title: "ゴール",
     detail: "その問題・課題に対してどうなったらゴール、達成といえるか",
   },
-  { id: 3, title: "きっかけ", detail: "その問題となった原因・きっかけは何か" },
+  { id: 3, title: "現状", detail: "現状どういう状況か" },
+  { id: 4, title: "打ち手", detail: "何をするのか" },
+  { id: 5, title: "効果・影響・障害", detail: "どういう効果があるのか" },
+  { id: 6, title: "流れ", detail: "どういう流れで進めていくか" },
+  { id: 7, title: "体制", detail: "どういう体制で進める？" },
+  { id: 8, title: "その他", detail: "その他考える論点は" },
 ];
