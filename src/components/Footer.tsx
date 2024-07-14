556bebf315d388a2fc2888f3940685a7b0c5e969
aion0721 <rudolf.aion@gmail.com>
2024-07-14 14:29:59 +0900

    ui: github add headers, link header, fix footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,7 +3,7 @@ import { Box } from "@chakra-ui/react";
 
 const Footer = React.memo(() => (
   <Box bg="gray.100" py={4} px={6} textAlign="center">
-    <p>Footer</p>
+    <p>aion</p>
   </Box>
 ));
 
