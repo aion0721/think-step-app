4d0c64e5cd403ca4f30d09d7f3b45f161673b4af
aion0721 <rudolf.aion@gmail.com>
2024-07-14 16:40:50 +0900

    feat: add stepper

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,17 @@
 import React, { useState, useCallback } from "react";
-import { Flex, VStack } from "@chakra-ui/react";
+import {
+  Box,
+  Flex,
+  Step,
+  StepIcon,
+  StepIndicator,
+  StepNumber,
+  Stepper,
+  StepSeparator,
+  StepStatus,
+  StepTitle,
+  VStack,
+} from "@chakra-ui/react";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import { questions, Question } from "./questions";
@@ -22,17 +34,34 @@ const App: React.FC = () => {
     },
     [currentQuestionIndex]
   );
-  const handleBeforeQuestion = useCallback(() => {
+  const handleBeforeQuestion = () => {
     setCurrentQuestionIndex((prevIndex) => prevIndex - 1);
-  }, []);
-  const handleNextQuestion = useCallback(() => {
+  };
+  const handleNextQuestion = () => {
     setCurrentQuestionIndex((prevIndex) => prevIndex + 1);
-  }, []);
+  };
 
   return (
     <Flex flexDirection="column" minHeight="100vh">
       <Header />
       <VStack flex={1} p={6} spacing={4}>
+        <Stepper index={currentQuestionIndex} w="100%">
+          {questions.map((question, index) => (
+            <Step key={index}>
+              <StepIndicator>
+                <StepStatus
+                  complete={<StepIcon />}
+                  incomplete={<StepNumber />}
+                  active={<StepNumber />}
+                />
+              </StepIndicator>
+              <Box flexShrink="0">
+                <StepTitle>{question.title}</StepTitle>
+              </Box>
+              <StepSeparator />
+            </Step>
+          ))}
+        </Stepper>
         {currentQuestionIndex < questions.length ? (
           <QuestionCard
             question={currentQuestion}
